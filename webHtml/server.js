6f3409654985e83c03f27d5ed83348f1b4c87084
이채민 <tancheonn@gmail.com>
2023-04-03 20:19:13 +0900

    중간체크

--- webHtml/server.js
+++ webHtml/server.js
@@ -1,28 +1,6 @@
-
-var animation = function (){
-    var items, winH;
-
-    var initModule = function() {
-        items = document.querySelectorAll(".text");
-        winH = window.innerHeight;
-        _addEventHandlers();
-    }
-
-    var _addEventHandlers = function(){
-        window.addEventListener("scroll", _checkPosition);
-        window.addEventListener("load", _checkPosition);
-        window.addEventListener("resize", initModule);
-    };
-
-    var _checkPosition = function(){
-        for(var i = 0; i < items.length; i++){
-            var posFromTop = items[i].getBoundingClientRect().top;
-            if(winH > posFromTop){
-                items[i].classList.add("active");
-            }
-        }
-    }
-    return{
-        init : initModule
-    }
-}
+$(document).ready(function(){
+    $('.more-btn').click(function(){
+        $(this).toggleClass('active');
+        $('.mainMenu').toggleClass('active');
+    })
+});
