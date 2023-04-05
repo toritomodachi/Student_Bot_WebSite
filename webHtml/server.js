7149c679f4c54d06d536bdb30cd5504091c7fd57
이채민 <tancheonn@gmail.com>
2023-04-05 12:51:35 +0900

    포지션순서 바꿈

--- webHtml/server.js
+++ webHtml/server.js
@@ -9,17 +9,17 @@ $(document).ready(function(){
     $('.container > video').fadeIn(2000);
 })
 
-let observer = new IntersectionObserver((e) => {
-    e.forEach((박스)=>{
-        if(박스.isIntersecting){
-            박스.target.style.opacity = 1;
-        } 
-        박스.intersectionRatio
-    })
-})
+// let observer = new IntersectionObserver((e) => {
+//     e.forEach((박스)=>{
+//         if(박스.isIntersecting){
+//             박스.target.style.opacity = 1;
+//         } 
+//         박스.intersectionRatio
+//     })
+// })
 
-let div = document.querySelectorAll('video')
-observer.observe(Video[0])
-observer.observe(Video[1])
-observer.observe(Video[2])
-observer.observe(Video[3])
+// let div = document.querySelectorAll('video')
+// observer.observe(Video[0])
+// observer.observe(Video[1])
+// observer.observe(Video[2])
+// observer.observe(Video[3])
