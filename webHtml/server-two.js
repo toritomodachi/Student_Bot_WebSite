442debb5779d6068c643f1a5d6d6e0fa64865cb5
이채민 <tancheonn@gmail.com>
2023-04-12 03:19:37 +0900

    아직 단점이 많음

---
+++ webHtml/server-two.js
@@ -0,0 +1,152 @@
+function doDisplay(){
+    var con = document.getElementById("content");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    } else {
+        con.style.display = "none";
+    }
+}
+function doDisplay2(){
+    var con = document.getElementById("content-two");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    } else {
+        con.style.display = "none";
+    }
+}
+function doDisplay3(){
+    var con = document.getElementById("content-three");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    } else {
+        con.style.display = "none";
+    }
+}
+function doDisplay4(){
+    var con = document.getElementById("content-four");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    } else {
+        con.style.display = "none";
+    }
+}
+function doDisplay5(){
+    var con = document.getElementById("content-five");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    } else {
+        con.style.display = "none";
+    }
+}
+function doDisplay6(){
+    var con = document.getElementById("content-six");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    } else {
+        con.style.display = "none";
+    }
+}
+function doDisplay7(){
+    var con = document.getElementById("content-seven");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    } else {
+        con.style.display = "none";
+    }
+}
+function doDisplay8(){
+    var con = document.getElementById("content-eight");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    } else {
+        con.style.display = "none";
+    }
+}
+function doDisplay9(){
+    var con = document.getElementById("content-nine");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    } else {
+        con.style.display = "none";
+    }
+}
+function doDisplay10(){
+    var con = document.getElementById("content-ten");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    } else {
+        con.style.display = "none";
+    }
+}
+function doDisplay11(){
+    var con = document.getElementById("content-eleven");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    } else {
+        con.style.display = "none";
+    }
+}
+function doDisplay12(){
+    var con = document.getElementById("content-twelve");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    } else {
+        con.style.display = "none";
+    }
+}
+function doDisplay13(){
+    var con = document.getElementById("content-thirteen");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    } else {
+        con.style.display = "none";
+    }
+}
+function doDisplay14(){
+    var con = document.getElementById("content-fourteen");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    } else {
+        con.style.display = "none";
+    }
+}
+function doDisplay15(){
+    var con = document.getElementById("content-fifteen");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    } else {
+        con.style.display = "none";
+    }
+}
+function doDisplay16(){
+    var con = document.getElementById("content-sixteen");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    } else {
+        con.style.display = "none";
+    }
+}
+function doDisplay17(){
+    var con = document.getElementById("content-seventeen");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    } else {
+        con.style.display = "none";
+    }
+}
+function doDisplay18(){
+    var con = document.getElementById("content-eighteen");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    } else {
+        con.style.display = "none";
+    }
+}
+function doDisplay19(){
+    var con = document.getElementById("content-nineteen");
+    if(con.style.display == "none"){
+        con.style.display = "block";
+    } else {
+        con.style.display = "none";
+    }
+}
