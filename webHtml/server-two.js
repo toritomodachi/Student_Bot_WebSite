004541f351eed9dcf562a0ed113590f6a8918cb6
이채민 <tancheonn@gmail.com>
2023-04-13 10:44:29 +0900

    웹반응형 버튼 추가 아무래도 테스트 필요

--- webHtml/server-two.js
+++ webHtml/server-two.js
@@ -5,5 +5,12 @@ function doDisplay(page){
         x[page-1].style.display = "block";
     }
 }
+
+$(document).ready(function(){
+    $('.more-btn').click(function(){
+        $(this).toggleClass('active');
+        $('.mainMenu').toggleClass('active');
+    })
+});
     
     
