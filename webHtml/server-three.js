49679611280df96f3f4eb6d3c59fc4ed922f63cc
이채민 <tancheonn@gmail.com>
2023-04-23 10:23:16 +0900

    다시 만드는중

--- webHtml/server-three.js
+++ webHtml/server-three.js
@@ -4,28 +4,28 @@ $(document).ready(function(){
         $('.mainMenu').toggleClass('active');
     })
 });
-window.addEventListener("wheel", function(e){
-    e.preventDefault();
-},{passive : false});
+// window.addEventListener("wheel", function(e){
+//     e.preventDefault();
+// },{passive : false});
 
-var mHtml = $("html");
-var page = 1;
+// var mHtml = $("html");
+// var page = 1;
 
 
-mHtml.animate({scrollTop : 0},10);
+// mHtml.animate({scrollTop : 0},10);
 
-$(window).on("wheel", function(e) {
-    if(mHtml.is(":animated")) return;
-    if(e.originalEvent.deltaY > 0) {
-        if(page == 40) return;
-        page++;
-    } else if(e.originalEvent.deltaY < 0) {
-        if(page == 1) return;
-        page--;
-    }
-    var posTop =(page-1) * $(window).height();
-    mHtml.animate({scrollTop : posTop});
-})
+// $(window).on("wheel", function(e) {
+//     if(mHtml.is(":animated")) return;
+//     if(e.originalEvent.deltaY > 0) {
+//         if(page == 40) return;
+//         page++;
+//     } else if(e.originalEvent.deltaY < 0) {
+//         if(page == 1) return;
+//         page--;
+//     }
+//     var posTop =(page-1) * $(window).height();
+//     mHtml.animate({scrollTop : posTop});
+// })
 
 // const options = {
 //     root: null, // viewport
